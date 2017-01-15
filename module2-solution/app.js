590183c7dc14c90129b46381a0788aae7cfaba05
Nishanth2143 <nishanth2143@coursera.com>
2017-01-15 21:00:16 +0530

    Second Assignment

---
+++ module2-solution/app.js
@@ -0,0 +1,84 @@
+(function(){
+  'use strict';
+
+  angular.module("ShoppingListCheckOff",[])
+    .controller('ToBuyController',ToBuyController)
+    .controller('AlreadyBoughtController',AlreadyBoughtController)
+    .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService){
+        var toBuy = this;
+        toBuy.listOfItems = ShoppingListCheckOffService.getToBuyItems();
+        toBuy.Bought = function(itemIndex){
+          try{
+              ShoppingListCheckOffService.addItem(itemIndex);
+            }
+            catch(error)
+            {
+              toBuy.empty = error.message;
+            }
+
+        };
+    }
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService){
+        var alreadyBought = this;
+            alreadyBought.listOfItems = ShoppingListCheckOffService.getBoughtItems();
+    }
+
+    function ShoppingListCheckOffService(){
+      var service = this;
+
+      var tobuy = [
+        {
+       name: "Milk",
+       quantity: "2"
+       },
+       {
+         name: "Donuts",
+         quantity: "200"
+       },
+       {
+         name: "Cookies",
+         quantity: "300"
+       },
+       {
+         name: "Chocolate",
+         quantity: "5"
+       },
+       {
+         name: "Peanut Butter",
+         quantity: "20"
+       }
+     ];
+      var bought = [];
+      var error = true;
+      service.getToBuyItems = function(){
+        return tobuy;
+      };
+
+      service.addItem = function(itemIndex){
+        debugger;
+
+          var item = tobuy.slice(itemIndex,itemIndex+1);
+          tobuy.splice(itemIndex, 1);
+          bought.push(item[0]);
+          if(bought.length != 0)
+          {
+            error = false;
+          }
+          if(tobuy.length ==0)
+          {
+            throw new Error("Everything is bought!");
+          }
+
+      };
+
+      service.getBoughtItems = function(){
+          return bought;
+      };
+
+    }
+})();
